b675bfb75d1bc1c2cd5e2f6731c07b2ccd5029e7
isaac <isanga@iastate.edu>
2018-09-08 20:18:22 -0500

    Google

--- Experiments/Isaac_Sanga/app/src/main/java/com/example/isaacsanga/demo1/Main2Activity.java
+++ Experiments/Isaac_Sanga/app/src/main/java/com/example/isaacsanga/demo1/Main2Activity.java
@@ -1,7 +1,14 @@
 package com.example.isaacsanga.demo1;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.net.URL;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +16,17 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button googlBtn = findViewById(R.id.googleBtn);
+        googlBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String google = "http://www.google.com";
+                Uri url = Uri.parse(google);
+                Intent toGoogle = new Intent(Intent.ACTION_VIEW, url);
+                if(toGoogle.resolveActivity(getPackageManager())!=null)
+                    startActivity(toGoogle);
+
+            }
+        });
     }
 }
