3e3bb8c05723f3f3ff6abe442afe6936f8449d7e
Tenson <tensonc@iastate.edu>
2018-09-09 16:07:10 -0500

    Phone call app

--- Experiments/TensonCai/Firstapp/app/src/main/java/com/example/tensoncai/firstapp/MainActivity.java
+++ Experiments/TensonCai/Firstapp/app/src/main/java/com/example/tensoncai/firstapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tensoncai.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity
     private EditText Password;
     private TextView Info;
     private Button Login;
+    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,21 +20,45 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Name = (EditText) findViewByID(R.id.etName);
-        Password = (EditText) findViewByID(R.id.etPassword);
-        Info = (TextView) findViewById(R.id.tvinfo);
-        Login = (EditText) findViewByID(R.id.LoginButton);
+        Name = (EditText)findViewByID(R.id.etName);
+        Password = (EditText)findViewByID(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvinfo);
+        Login = (Button)findViewByID(R.id.LoginButton);
 
+        Info.setText("No of attempts remaining: 5");
+
+        Login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        }
+        );
 
 
 
     }
 
-    private void validate(String userName, String password)
+    private void validate(String userName, String userPassword)
     {
-        if (userName == "Admin" && password == "1234")
+        if ((userName == "Admin") && (userPassword == "1234"))
         {
+            // intent is used to move from one activity to another activity
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+        else
+        {
+            counter--;
+
+            Info.setText("No of attempts remaining: " + String.valueOf(counter));
+
+            if (counter == 0)
+            {
+                Login.setEnabled(false);
+            }
         }
     }
 }
