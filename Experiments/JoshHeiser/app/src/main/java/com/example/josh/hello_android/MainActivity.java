46f79341eab77992c50403aac4469567b2cd53ed
Josh Heiser <jheiser@iastate.edu>
2018-09-10 22:42:07 -0500

    8 ball App part 1

--- Experiments/JoshHeiser/app/src/main/java/com/example/josh/hello_android/MainActivity.java
+++ Experiments/JoshHeiser/app/src/main/java/com/example/josh/hello_android/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.josh.hello_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1, b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.buttonPlay);
+        b2 = (Button) findViewById(R.id.buttonAbout);
+
+        b2.setOnClickListener(new View.onClickListener(){
+            @Override
+            public void OnClick(View view){
+                Toast.makeText(getApplicationContext(), "This app was created in 2018 by Josh Heiser", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Magic 8 Ball was Created by Mattel Coporation, all Rights reserved", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
