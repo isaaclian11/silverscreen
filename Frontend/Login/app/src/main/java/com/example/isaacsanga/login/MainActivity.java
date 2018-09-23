9392257d72dfe25c294a7c111fa32ac5e993ae46
isaac <isanga@iastate.edu>
2018-09-23 01:00:48 -0500

    Login

---
+++ Frontend/Login/app/src/main/java/com/example/isaacsanga/login/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.isaacsanga.login;
+
+import android.app.DownloadManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText getEmail, getPassword;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getEmail = (EditText) findViewById(R.id.getEmail);
+        getPassword = (EditText) findViewById(R.id.getPassword);
+        login = (Button) findViewById(R.id.login);
+
+
+    }
+
+    private void login(){
+        String url = "";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return super.getParams();
+            }
+        };
+
+    }
+
+}
+
+
