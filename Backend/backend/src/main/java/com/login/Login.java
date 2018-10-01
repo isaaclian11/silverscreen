ccc0bf2123dd436d25561abb4fbc91c700b93755
Josh Heiser <jheiser@iastate.edu>
2018-09-30 19:41:12 -0500

    adding more functionality

--- Backend/backend/src/main/java/com/login/Login.java
+++ Backend/backend/src/main/java/com/login/Login.java
@@ -1,23 +1,33 @@
 package com.login;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
 
 @Entity
 @Table (name = "Login")
 public class Login {
 	@Id
-	@GeneratedValue = (strategy = GenerationType.IDENTITY);
-	//gets the number for each row
-	private integer id;
-	
-	//gets the row of usernames
-	@NotEmpty
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column (name = "username")
+	@NotFound(action = NotFoundAction.EXCEPTION)
 	private String username;
 	
 	//gets the row of passwords
-	@NotEmpty
+	@Column (name = "password")
+	@NotFound(action = NotFoundAction.EXCEPTION)
 	private String password;
 
+	public String getUserName() {
+		return username;
+	}
+	public String getPassword() {
+		return password;
+	}
 }
--- Backend/backend/src/main/java/com/login/LoginController.java
+++ Backend/backend/src/main/java/com/login/LoginController.java
@@ -1,12 +1,34 @@
 package com.login;
 
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 
 @Controller
 public class LoginController {
 	@Autowired
-	private LoginRepository loginrepository;
+	private LoginRepository loginsRepository;
 	
+	private final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	
+	@RequestMapping(method = RequestMethod.POST, path = "/Login")
+	public String checkUserName(@PathVariable("username") String username) {
+		logger.info("Entered the controller layer");
+		String results = LoginRepository.findByUsername(username);
+		return results;
+	}
+	@RequestMapping(method = RequestMethod.POST, path = "/Login")
+	public String checkPassword(@PathVariable("password") String password) {
+		logger.info("Entered the controller layer");
+		String result = LoginRepository.findByPassword(password);
+	}	
+	/*
+	@Requestmapping(method = RequestMethod.GET, path = "/Login")
+	public String addUser(Login user, password) {
+		LoginRepository.addUser(user, password)
+	}
+	*/
 
 }
