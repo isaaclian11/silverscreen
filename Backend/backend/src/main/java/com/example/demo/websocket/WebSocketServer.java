b57ef81c466375db366e25f15c6800bdfaaeb0d4
Josh Heiser <jheiser@iastate.edu>
2018-10-31 10:07:36 -0500

    Added Websocket classes

---
+++ Backend/backend/src/main/java/com/example/demo/websocket/WebSocketServer.java
@@ -0,0 +1,47 @@
+package com.example.demo.websocket;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+
+public class WebSocketServer {
+	private Session session;
+	private static Set<WebSocketServer> chatEndPoints = new CopyOnWriteArraySet<>();
+	private static Map<String, String> users = new HashMap();
+	
+	@OnOpen
+	public void onOpen(Session session, @PathParam("username") String username) throws IOException() {
+		this.session = session;
+		chatEndPoints.add(this);
+		users.put(session.getId(), username);
+		//String message = "User " + username + " has joined the chat" 
+		//broadcast(message);
+	}
+	public void onMessage(Session session, String message) throws IOException() {
+		sendMessagetoAPraticularUser(session,echo); //change this to a speciifc person?
+		
+	}
+	public void onClose() {
+		chatEndPoints.remove(this);
+		String message = "user has left the chat";
+		broadcast(message);
+		
+	}
+	public void sendMessageToAPractiuclarUser(Session session, String message) {
+		
+	}
+	public static void broadcast() {
+		
+	}
+
+}
